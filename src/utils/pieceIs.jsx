0a469e81bd35473ec61fc457fe8356d81bcb5ef2
Edwin Divine <omegbuedwin@gmail.com>
2025-05-12 18:36:12 +0100

    e don tey

--- src/utils/pieceIs.jsx
+++ src/utils/pieceIs.jsx
@@ -1,19 +1,19 @@
 function pieceIs(i,set) {
     if (set=='w') {
         if (i==0 || i==7) return `rook`
-        // if (i==1||i==6) return `knight`
-        // else if (i==2||i==5) return `bishop`
-        // else if (i==3) return `queen`
+        if (i==1||i==6) return `knight`
+        else if (i==2||i==5) return `bishop`
+        else if (i==3) return `queen`
         else if (i==4) return `king`
-        // else if (i>7) return `pawn`
+        else if (i>7) return `pawn`
     }
     else{
         if(i==0 || i==7) return `b_rook`
-        // else if (i==1||i==6) return `b_knight`
-        // else if (i==2||i==5) return `b_bishop`
-        // else if (i==3) return `b_queen`
+        else if (i==1||i==6) return `b_knight`
+        else if (i==2||i==5) return `b_bishop`
+        else if (i==3) return `b_queen`
         else if (i==4) return `b_king`
-        // else if (i>7) return `b_pawn`
+        else if (i>7) return `b_pawn`
     }
 }
 export default pieceIs
